31ee7e3f778c2c34da716f7717fbd465c4654902
Clifton Hudson <clifrunsalot@gmail.com>
2024-12-19 22:50:08 +1000

    Learning MERN

--- devconnector/config/passport.js
+++ devconnector/config/passport.js
@@ -1,3 +1,12 @@
+/**
+ * This module configures the passport middleware for authenticating requests.
+ */
+
+/////////////
+//         // 
+// IMPORTS //
+//         //
+/////////////
 
 // Importing passport-jwt to authenticate requests
 const JwtStrategy = require('passport-jwt').Strategy;
@@ -8,15 +17,27 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 // Importing mongoose to interact with the database
 const mongoose = require('mongoose');
 
-// Load User model
+// Importing passport to authenticate requests
+const passport = require('passport');
+
+//////////////
+//          // 
+// DATABASE //
+//          //
+//////////////
+
+// Loads User model
 // 'User' refers to the model, aka the collection called 'users' in the database.
 const User = require('../models/User');
 
+////////////////////
+//                // 
+// passport logic //
+//                //
+////////////////////
+
 // Importing the keys to be used in the app
 const keys = require('./keys');
-
-// Importing passport to authenticate requests
-const passport = require('passport');
 
 // Options for the passport-jwt strategy
 const opts = {
--- devconnector/server.js
+++ devconnector/server.js
@@ -1,49 +1,102 @@
+/**
+ * This is the entry point of the application.
+ * This module does the following:
+ * 1. Imports the necessary modules to run the application.
+ * 2. Connects to the database.
+ * 3. Configures the server to listen on port 5000.
+ * 4. Uses body-parser to parse incoming requests.
+ * 5. Uses passport to authenticate requests.
+ * 6. Routes requests to the appropriate route.
+ */
 
-// Importing express to use framework
+/////////////
+//         // 
+// IMPORTS //
+//         //
+/////////////
+
+// express Framework 
 const express = require('express');
 
-// Importing mongoose to connect to the database
+// mongoose for DB interaction 
 const mongoose = require('mongoose');
 
-// Importing body-parser to parse incoming requests
+// body-parser for parsing incoming requests
 const bodyParser = require('body-parser');
 
-// Importing passport to authenticate requests
+// passport for authenticating requests
 const passport = require('passport');
+
+//////////////
+//          // 
+// DATABASE //
+//          //
+//////////////
+
+// Imports database configuration
+const db = require('./config/keys').mongoURI;
+
+// Connects to database
+mongoose.connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+/////////////
+//         // 
+// express //
+//         //
+/////////////
+
+// Creates an instance
+const app = express();
+
+////////////////
+//            // 
+// body-parse //
+//            //
+////////////////
+
+// Instructs app to use body-parser 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//////////////
+//          // 
+// passport //
+//          //
+//////////////
+
+// Instructs app to use passport 
+app.use(passport.initialize());
+
+// Configures passport to authenticate requests
+// The logic for this is in config/passport.js 
+require('./config/passport')(passport);
+
+////////////
+//        // 
+// ROUTES //
+//        //
+////////////
 
 // Route abstractions 
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const users = require('./routes/api/users');
 
-// Importing the database configuration
-const db = require('./config/keys').mongoURI;
-
-// Connecting to the database
-mongoose.connect(db)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
-
-// Creating an instance of express
-const app = express();
-
-// Using body-parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// Passport middleware
-app.use(passport.initialize());
-
-// Passport Config
-require('./config/passport')(passport);
-
-// Route requests to the appropriate route
+// Routes requests to the appropriate endpoint 
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 app.use('/api/users', users);
 
-// Setting up the server to listen on port 5000
+////////////
+//        // 
+// SERVER //
+//        //
+////////////
+
+// Sets up server to listen on port 5000
 const port = process.env.PORT || 5000;
 
-// Starting the server
+// Starts the server
 app.listen(port, () => { console.log(`Server  is running on port ${port}`) });
